fd570ff2c12d8d6a245519a870fdddbcfdeb8acf
Venkateswara Rao Mandela <venkat.mandela@gmail.com>
2019-05-26 17:08:53 +0530

    site template update

--- js/apply-styles.js
+++ js/apply-styles.js
@@ -1,13 +1,32 @@
 $( document ).ready(function() {
  
-    $("table").addClass("table table-bordered");
-    $("p").addClass("text-jusitfy");
-    $("#header ul li").addClass("text-capitalize h2");
+    // bootstrap needs blockquote class
+    // on the tag to apply styles
+    $("blockquote").addClass("blockquote");
 
-    $("#sidebar ul").addClass("nav");
-    $("#sidebar > ul").addClass("bs-docs-sidenav affix");
+    // Justify the paragraphs
+    $("p").addClass("text-justify");
 
-    $("img").addClass("img-responsive center-block");
+    // Slight shadow for highlighting code
+    $("div.sourceCode").addClass("shadow-sm p-3 mb-5");
+
+    $("div.sidebar ul").addClass("nav flex-column");
+    
+    $("div.sidebar ul li").addClass("nav-item");
+    $("div.sidebar ul li a").addClass("nav-link");
+
+
+    //$("div.navmenu").addClass("navbar-collapse collapse");
+    $("div.navmenu ul").addClass("nav nav-pills");
+    $("div.navmenu ul li").addClass("nav-item");
+    $("div.navmenu ul li a").addClass("nav-link");
+    //$("table").addClass("table table-bordered");
+    // $("#header ul li").addClass("text-capitalize h2");
+
+    // $("#sidebar ul").addClass("nav");
+    // $("#sidebar > ul").addClass("bs-docs-sidenav affix");
+
+    // $("img").addClass("img-responsive center-block");
 
     $('.grid').masonry({
         // options
@@ -17,6 +36,7 @@ $( document ).ready(function() {
         percentPosition: true
     });
 
+    $("body").css("position","relative","margin-top","5em");
 //    $("caption").css("color","#000000");
 //    $("caption").css("text-align", "center");
 //    $("caption").css("font-weight", "bold");
